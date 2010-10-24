a64f9988c2a58f87b1e7352325467ff4955c0266
roguebean <roguebean@3eb8c50c-0d06-4a70-732b-4ba95e2fca1e>
2010-10-24 09:59:18 +0000

    Changed looks a little.

--- trunk/src/seoeun/saveme/InfoActivity.java
+++ trunk/src/seoeun/saveme/InfoActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,36 +15,45 @@ public class InfoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info);
         
+        // get information
         int descriptionRes;
         String version;
-        
         PackageInfo info;
         try {
             info = getPackageManager().getPackageInfo(getPackageName(), 0);
         } catch(NameNotFoundException e) {
             info = null;
         }
-        
         if(info != null) {
             descriptionRes = info.applicationInfo.descriptionRes;
             version = info.versionName;
         } else {
-            descriptionRes = 0;
-            version = null;
+            descriptionRes = R.string.app_description;
+            version = "unknown";
         }
         
-        ((ImageView) findViewById(R.id.about_icon)).setImageResource(R.drawable.app_icon);
-        ((TextView) findViewById(R.id.about_name)).setText(R.string.app_name);
-        if(version != null) {
-            ((TextView) findViewById(R.id.about_version)).setText(version);
-        }
-        if(descriptionRes > 0) {
-            ((TextView) findViewById(R.id.about_description)).setText(descriptionRes);
-        }
+        // get views
+        ImageView vIcon = (ImageView) findViewById(R.id.about_icon);
+        TextView vName = (TextView) findViewById(R.id.about_name);
+        TextView vVersion = (TextView) findViewById(R.id.about_version);
+        TextView vDescription = (TextView) findViewById(R.id.about_description);
+        TextView vLink = (TextView) findViewById(R.id.about_link);
+        TextView vCopyright = (TextView) findViewById(R.id.about_copyright);
+        TextView vContact = (TextView) findViewById(R.id.about_contact);
         
-        ((TextView) findViewById(R.id.about_link)).setText(R.string.app_link);
-        ((TextView) findViewById(R.id.about_copyright)).setText(R.string.app_copyright);
-        ((TextView) findViewById(R.id.about_contact)).setText(R.string.app_contact);
+        // set values
+        vIcon.setImageResource(R.drawable.app_icon);
+        vName.setText(R.string.app_name);
+        vVersion.setText(version);
+        vDescription.setText(descriptionRes);
+        vLink.setText(R.string.app_link);
+        vCopyright.setText(R.string.app_copyright);
+        vContact.setText(R.string.app_contact);
+        
+        // linkify
+        Linkify.addLinks(vLink, Linkify.ALL);
+        Linkify.addLinks(vCopyright, Linkify.ALL);
+        Linkify.addLinks(vContact, Linkify.ALL);
     }
 
 }
